e38e340091ccaa5db27240654d4fd13ce838a555
Natalia Jagiello <jagiello.nat@gmail.com>
2018-09-28 14:09:19 +0200

    AppNavigation

--- mwcountdown/App.js
+++ mwcountdown/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { YellowBox } from 'react-native';
 import EventList from './EventList';
+import { createStackNavigator } from 'react-navigation';
+import EventForm from './EventForm';
+//import { createBottomTabNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <EventList/>
-    );
+/*
+export default createBottomTabNavigator({
+  Home: EventList,
+//  Settings: SettingsScreen,
+});
+*/
+
+export default createStackNavigator ({
+  list: {
+    screen: EventList,
+    navigationOptions: () => ({
+      title: 'Coming soon...',
+    }),
+  },
+  form: {
+    screen: EventForm,
+    navigationOptions: () => ({
+      title: 'Add an event'
+    })
   }
-}
-
+})
--- mwcountdown/EventList.js
+++ mwcountdown/EventList.js
@@ -1,17 +1,10 @@
 import React, {Component} from 'react';
 import {FlatList, Text, View, StyleSheet} from 'react-native';
 import EventCard from './EventCard';
+import ActionButton from 'react-native-action-button'
 
 const styles = StyleSheet.create({
-    content: {
-        marginTop: 20,  
-        flex: 1,   
-    },
-    contentHeader: {
-        textAlign: 'center',
-        color: 'black',
-        fontSize: 30,
-    }
+
 });
 
 class EventList extends Component {
@@ -23,8 +16,7 @@ class EventList extends Component {
         setInterval(() => {
             this.setState({
                 events: this.state.events.map(evt => ({
-                    ...evt,
-                    timer: Date.now(),
+                    ...evt
                 })),
             });
         }, 1000);
@@ -37,16 +29,13 @@ class EventList extends Component {
     }
 
     render() {
-        return (
-            <View style={styles.content}> 
-            <Text style={styles.contentHeader}>Coming soon...</Text>
+        return [
             <FlatList
                 data={this.state.events}
                 renderItem={({ item }) => <EventCard event={item}/>}
                 keyExtractor={item => item.id}
             />
-            </View>
-        );
+        ];
     }
 }
 
